24ab0bb6cc6f4c834066ed81c75196b87221ecd1
Dmitrii Kabanov <kabanov-dm@zvuk.com>
2024-04-23 14:09:35 +0300

    lalala test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Button from '../components/Button/Button';
-import Card from '../components/Card/Card';
-import '../../public/global.css';
-
+import React from "react";
+import Button from "../components/Button/Button";
+import Card from "../components/Card/Card";
+import "../../public/global.css";
 
 function MyApp() {
-    return (
-      <>
-        <Card />
-        <Button />
-      </>
-    );
-};
-  
+  return (
+    <>
+      <Card />
+      asdasdasd
+      <Button />
+    </>
+  );
+}
+
 export default MyApp;
