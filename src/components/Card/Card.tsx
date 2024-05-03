732042fb9e670ba768071ca3607ac52d7ca07fb2
Nikita Sivakov <sivakov-na@zvuk.com>
2024-05-03 12:58:25 +0300

    update Card and Cards components

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Card.module.css';
 
-function Card ({text = "для кафе"}) {
+function Card ({text="для кафе", palylistSrc }) {
     return (
     <>
         <div className={styles.background}>
@@ -10,7 +10,10 @@ function Card ({text = "для кафе"}) {
                             <p className={styles.text_container_heading_2}>Музыка</p>
                             <p className={styles.text_container_heading_3}>{text}</p>
                         </div>
-                    <div className={styles.playlist_image}></div>
+                        <img 
+                            src={palylistSrc} 
+                            className={styles.playlist_image}>
+                        </img>
                 </div>
         </div>
     </>
--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -4,9 +4,9 @@ import styles from "./Gallery.module.css";
 function Gallery () {
     return (
         <div className={styles.cards_container}>
-            <Card />
-            <Card />
-            <Card />
+            <Card text={"для кафе"} palylistSrc={"./images/playlist_cards/pop_small.png"}/>
+            <Card text={"для барбершопа"} palylistSrc={"./images/playlist_cards/barbershop_small.png"}/>
+            <Card text={"для ритейла"} palylistSrc={"./images/playlist_cards/shop_small.png"}/>
         </div>
     )
 };
