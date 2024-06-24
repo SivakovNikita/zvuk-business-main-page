861eb09a726b8cd8c04e0036f6551ec1d4e3ba7a
Nikita Sivakov <sivakov-na@zvuk.com>
2024-06-24 13:50:29 +0300

    improvments in Header, List and other

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,123 +1,99 @@
 import { useState } from 'react';
 import Link from 'next/link';
+import List from '../List/List';
 import Image from 'next/image';
 import logo from '../../../public/images/logo/zvuk_business_logo.png';
 import Button from '../Button/Button';
 import styles from './Header.module.scss';
-import { BiSolidPhoneCall } from "react-icons/bi";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { MdOutlineArrowLeft } from "react-icons/md";
-import { IoMdArrowDropdown } from "react-icons/io";
+import { BiSolidPhoneCall } from 'react-icons/bi';
+import { businessTypesData, toolsData, aboutData } from './constants';
+import { IoMdArrowDropdown } from 'react-icons/io';
+import clsx from 'clsx';
 
 const Header = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    return (
-        <header className={styles.header}>
-            
-            {/* логотип и иконка бургер */}
-            <div className={styles.mobile_container}>
-                <div className={styles.logo}>
-                    <Link href="/">
-                        <Image src={logo} alt="Звук Бизнес" width={132} />
-                    </Link>
-                </div>
-                <div
-                    className={styles.burger_menu}
-                    onClick={toggleMenu}
-                    aria-expanded={isMenuOpen}
-                    aria-controls="primary-navigation"
-                    role="button"
-                >
-                    <GiHamburgerMenu />
-                </div>
+  return (
+    <header className={styles.header}>
+      {/* логотип и иконка бургер */}
+      <div className={styles.mobile_container}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <Image src={logo} alt="Звук Бизнес" width={132} />
+          </Link>
+        </div>
+
+        <div
+          className={styles.burger_menu}
+          onClick={toggleMenu}
+          aria-expanded={isMenuOpen}
+          aria-controls="primary-navigation"
+          role="button"
+        >
+          <div className={clsx(styles.hamburger, { [styles.active]: isMenuOpen })}>
+            <span className={styles.line1}></span>
+            <span className={styles.line2}></span>
+            <span className={styles.line3}></span>
+          </div>
+        </div>
+      </div>
+
+      {/* Меню */}
+      <nav className={`${styles.nav} ${isMenuOpen ? styles.nav_open : ''}`}>
+        {/* Ссылки */}
+
+        <ul className={styles.links_container}>
+          <li>
+            <Link className={styles.menu_link} href="/">
+              Тарифы
+            </Link>
+          </li>
+
+          <li className={styles.nav_list_item}>
+            <Link className={styles.menu_link} href="/">
+              Типы бизнеса
+              <IoMdArrowDropdown className={styles.arrow} />
+            </Link>
+            <div className={styles.nav_list_item_content}>
+              <List items={businessTypesData} />
             </div>
-            
-            {/* Меню */}
-            <nav className={`${styles.nav} ${isMenuOpen ? styles.nav_open : ''}`} id="primary-navigation">
-                
-                {/* Ссылки */}
-                <div className={styles.links_container}>
-                <Link className={styles.menu_link} href="/">Тарифы</Link>
-                    <ul className={styles.menu}>
-                        <li className={styles.menu_item}>
-                            <a href="#!" className={styles.menu_link}>Типы бизнеса<IoMdArrowDropdown className={styles.arrow}/></a>
-                            <ul className={styles.dropdown_content}>
-                                <li className={styles.dropdown_item}>
-                                    <Link href="/">Ритейл<MdOutlineArrowLeft className={styles.arrow}/></Link>
-                                    <ul className={styles.sub_dropdown_content}>
-                                        <li><Link href="/">Магазин одежды</Link></li>
-                                        <li><Link href="/">Магазин электроники</Link></li>
-                                    </ul>
-                                </li>
-                                <li className={styles.dropdown_item}>
-                                    <Link href="/">HoReCa<MdOutlineArrowLeft className={styles.arrow}/></Link>
-                                    <ul className={styles.sub_dropdown_content}>
-                                        <li><Link href="/">Рестораны</Link></li>
-                                        <li><Link href="/">Кафе</Link></li>
-                                    </ul>
-                                </li>
-                                <li className={styles.dropdown_item}>
-                                    <Link href="/">Медицина<MdOutlineArrowLeft className={styles.arrow}/></Link>
-                                    <ul className={styles.sub_dropdown_content}>
-                                        <li><Link href="/">Клиники</Link></li>
-                                        <li><Link href="/">Аптеки</Link></li>
-                                    </ul>
-                                </li>
-                                <li className={styles.dropdown_item}>
-                                    <Link href="/">Красота<MdOutlineArrowLeft className={styles.arrow}/></Link>
-                                    <ul className={styles.sub_dropdown_content}>
-                                        <li><Link href="/">Барбершоп</Link></li>
-                                        <li><Link href="/">Салон Красоты</Link></li>
-                                    </ul>
-                                </li>
-                                <li className={styles.dropdown_item}>
-                                    <Link href="/">Спорт<MdOutlineArrowLeft className={styles.arrow}/></Link>
-                                    <ul className={styles.sub_dropdown_content}>
-                                        <li><Link href="/">Фитнес клуб</Link></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                
-                    <ul className={styles.menu}>
-                        <li className={styles.menu_item}>
-                            <a href="#!" className={styles.menu_link}>Инструменты<IoMdArrowDropdown className={styles.arrow}/></a>
-                            <ul className={styles.dropdown_content}>
-                                <li><Link href="/">Плеер</Link></li>
-                                <li><Link href="/">HiFi качество</Link></li>
-                                <li><Link href="/">ИИ ролик</Link></li>
-                                <li><Link href="/">Умная колонка SberBoom</Link></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul className={styles.menu}>
-                        <li className={styles.menu_item}>
-                            <a href="#!" className={styles.menu_link}>О нас<IoMdArrowDropdown className={styles.arrow}/></a>
-                            <ul className={styles.dropdown_content}>
-                                <li><Link href="/">Блог</Link></li>
-                                <li><Link href="/">Артистам</Link></li>
-                                <li><Link href="/">Партнерская программа</Link></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-                {/* Кнопки */}
-                <div className={styles.buttons_container}>
-                    <Link href="/">
-                        <BiSolidPhoneCall className={styles.phone_icon} />
-                    </Link>
-                    <Button text="Войти" className="nav_menu" />
-                    <Button text="Зарегистрироваться" />
-                </div>
-            </nav>
-        </header>
-    );
+          </li>
+
+          <li className={styles.nav_list_item}>
+            <Link className={styles.menu_link} href="/">
+              Инструменты
+              <IoMdArrowDropdown className={styles.arrow} />
+            </Link>
+            <div className={styles.nav_list_item_content}>
+              <List items={toolsData} />
+            </div>
+          </li>
+
+          <li className={styles.nav_list_item}>
+            <Link className={styles.menu_link} href="/">
+              О нас
+              <IoMdArrowDropdown className={styles.arrow} />
+            </Link>
+            <div className={styles.nav_list_item_content}>
+              <List items={aboutData} />
+            </div>
+          </li>
+        </ul>
+        {/* Кнопки */}
+        <div className={styles.buttons_container}>
+          <Link href="/">
+            <BiSolidPhoneCall className={styles.phone_icon} />
+          </Link>
+          <Button text="Войти" className="nav_menu" />
+          <Button text="Зарегистрироваться" />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
